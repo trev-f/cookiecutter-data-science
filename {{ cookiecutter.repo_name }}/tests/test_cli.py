b5cc6b3050726a676b6fb9913ddfcf13f753854b
Trevor F. Freeman <trevor.freeman@utexas.edu>
2022-10-21 13:31:48 -0400

    Add test for finalize dataset command

--- {{ cookiecutter.repo_name }}/tests/test_cli.py
+++ {{ cookiecutter.repo_name }}/tests/test_cli.py
@@ -2,6 +2,13 @@ from click.testing import CliRunner
 from {{ cookiecutter.repo_name }}.cli import cli
 
 
+def test_download_data():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["download-data"])
+    assert result.exit_code == 0
+    assert "Download external data!" in result.output
+
+
 def test_make_dataset():
     runner = CliRunner()
     result = runner.invoke(cli, ["make-dataset"])
@@ -9,8 +16,8 @@ def test_make_dataset():
     assert "Make dataset!" in result.output
 
 
-def test_download_data():
+def test_finalize_dataset():
     runner = CliRunner()
-    result = runner.invoke(cli, ["download-data"])
+    result = runner.invoke(cli, ["finalize-dataset"])
     assert result.exit_code == 0
-    assert "Download external data!" in result.output
+    assert "Finalize the dataset!" in result.output
