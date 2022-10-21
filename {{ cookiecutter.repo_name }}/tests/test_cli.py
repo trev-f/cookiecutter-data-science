d28a27658e08434035e2cd3052a2adba9ada8277
Trevor F. Freeman <trevor.freeman@utexas.edu>
2022-10-21 12:19:01 -0400

    Add CLI tests

---
+++ {{ cookiecutter.repo_name }}/tests/test_cli.py
@@ -0,0 +1,16 @@
+from click.testing import CliRunner
+from {{ cookicutter.repo_name }}.cli import cli
+
+
+def test_say_hello():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["say-hello"])
+    assert result.exit_code == 0
+    assert "Hello World" in result.output
+
+
+def test_make_dataset():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["make-dataset"])
+    assert result.exit_code == 0
+    assert "Make dataset!" in result.output
